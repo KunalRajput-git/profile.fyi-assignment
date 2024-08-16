2e0a3ca5f776b63c88882f51768d4e388332ae18
GokuSuperSaiyan72 <KunalRajput7656@gmail.com>
2024-08-16 10:56:04 +0530

    setup: added initial cart page

---
+++ src/app/cart/page.js
@@ -0,0 +1,7 @@
+export default function Cart() {
+  return (
+    <div className="pt-20 pb-6 md:pt-24 px-3 md:px-8 m-auto w-full md:w-[600px]">
+      {/* Cart will go here  */}
+    </div>
+  );
+}
